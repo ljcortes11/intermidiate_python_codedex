ff1d399dd4f29d9009f3bee2e40bdfedc046f1f6
ljcortes11 <150732294+ljcortes11@users.noreply.github.com>
2024-04-02 18:31:42 -0500

    Create fruit_card.py

---
+++ fruit_card.py
@@ -0,0 +1,12 @@
+# Write code below
+
+favorite_fruits = {'grapes','apples','banana','orange','lemon'}
+
+friends_fruits = {'strawberry','watermelon','papaya','lemon','apples'}
+
+combine_fruits = favorite_fruits.union(friends_fruits)
+same_fruits =  favorite_fruits.intersection(friends_fruits)
+
+
+print(same_fruits)
+print(combine_fruits)
